6dfdcd9e06c776d9b4b6446afe9ebbc4362348f6
Arslan Mushtaq <arslanark@gmail.com>
2023-02-22 10:33:42 +0500

    Create Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 function App() {
   return (
-    <div>
-      <h1>Welcome to RocketX</h1>
-    </div>
+    <>
+      {/* Header */}
+      <header>
+        <h1>RocketX</h1>
+      </header>
+      {/* Banner */}
+      <section>
+        <div>
+          <h2>The Title of Your Online Course</h2>
+          <h3>Learn how to do things faster and better</h3>
+          <p>
+            Create a compelling summary of your course content and the key
+            benefits for subscribers
+          </p>
+        </div>
+      </section>
+      {/* Search Bar */}
+      <section>
+        <form>
+          <input type="text" name="text" placeholder="Search Capsules" />
+          <input type="submit" name="submit" value="Search" />
+        </form>
+      </section>
+      {/* Data Grid */}
+      <section>
+        <div></div>
+      </section>
+    </>
   );
 }
 
